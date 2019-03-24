da4db701138a8121d142b5ea0c99d4a8706f75a5
Nelson Patrao <nelson.patrao@acadomia.fr>
2019-03-24 23:03:25 +0100

    fix json call

--- HttpTrigger12/index.js
+++ HttpTrigger12/index.js
@@ -1,29 +1,18 @@
-const axios = require("axios");
+var request = require("request");
 
-module.exports = async function(context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
-
-  if (req.query.id || (req.body && req.body.id)) {
-    axios
-      .get(
-        "https://www.acadomia.fr/webservices/?type=liste_agences&agence_id=" +
-          (req.query.id || req.body.id)
-      )
-      .then(response => {
-        //   console.log(response.data.url);
-        //   console.log(response.data.explanation);
+module.exports = function(context) {
+  request(
+    "https://www.acadomia.fr/webservices/?type=liste_agences&agence_id=3",
+    function(error, response, body) {
+      if (error) {
+        context.log(error);
+      }
+      if (!error && response.statusCode == 200) {
         context.res = {
-          // status: 200, /* Defaults to 200 */
-          body: response
+          body: JSON.parse(body)
         };
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  } else {
-    context.res = {
-      status: 400,
-      body: "Please pass a id on the query string or in the request body"
-    };
-  }
+      }
+      context.done();
+    }
+  );
 };
